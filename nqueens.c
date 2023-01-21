3d698c4279ad05243fadaa72b5dd4d9433c8d7a0
Anagha-Kalyani <119789101+Anagha-Kalyani@users.noreply.github.com>
2023-01-21 19:06:48 +0530

    Create nqueens.c

---
+++ nqueens.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int a[30],count=0;
+int place(int pos) {
+	int i;
+	for (i=1;i<pos;i++) {
+		if((a[i]==a[pos])||((abs(a[i]-a[pos])==abs(i-pos))))
+		   return 0;
+	}
+	return 1;
+}
+void print_sol(int n) {
+	int i,j;
+	count++;
+	printf("\n\nSolution #%d:\n",count);
+	for (i=1;i<=n;i++) {
+		for (j=1;j<=n;j++) {
+			if(a[i]==j)
+			    printf("Q\t"); 
+			else
+			    printf("*\t");
+		}
+		printf("\n");
+	}
+}
+void queen(int n) {
+	int k=1; //queen 1 ie row 1
+	a[k]=0;
+	while(k!=0) {
+		a[k]=a[k]+1; //next column
+		while((a[k]<=n)&&!place(k)){
+		   a[k]=a[k]+1; //if cant be placed in that col, then next col
+		}
+		if(a[k]<=n) {
+			if(k==n)
+			    print_sol(n); //sol found
+			else {
+				k++; //next row
+				a[k]=0;
+			}
+		} 
+		else{
+		   k--; //backtrack
+		}
+	}
+}
+void main() {
+	int i,n;
+	printf("Enter the number of Queens\n");
+	scanf("%d",&n);
+	if(n<=3){
+	    printf("not possible");
+	    return;
+	}
+	queen(n);
+	printf("\nTotal solutions=%d",count);
+}
